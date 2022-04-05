d35ef2e8638eece16679db0b08c71c90e1eb8770
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-04-05 14:00:02 +0200

    Remove unussed route

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,5 @@ router.use(require("../middlewares/token"));
 
 // Other routes _____________________________________
 router.use("/template", require("./template"));
-router.use("/", require("./redirect"));
 
 module.exports = router;
