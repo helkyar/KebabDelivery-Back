d673430881c86aa76212f6ded73cc907764402b3
josemsb92 <josemsb92@gmail.com>
2022-05-07 11:13:36 +0200

    patchOrder Response now return the full order instead data

--- controllers/orders/patchOrder.js
+++ controllers/orders/patchOrder.js
@@ -1,14 +1,13 @@
 const OrderManager = require(`../../${process.env.MANAGER}/OrderManager`);
 
-async function patchOrder(req, res) {
-  
+async function patchOrder(req, res) {  
   const id = req.params;
   const data = req.body;
   //(!) Validation
   const order = await OrderManager.update(data, id);
   //(!) Universal manager -> model response
   order !== null
-    ? res.status(200).json(data)
+    ? res.status(200).json(order)
     : res.status(400).json({ error: "Wrong format" });
 }
 module.exports = patchOrder;
