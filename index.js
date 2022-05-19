455258cdc7f8df76642697c3553d1a1c77bb8dfd
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-05-19 12:29:33 +0200

    Remove email library

--- index.js
+++ index.js
@@ -10,43 +10,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(require("express").json());
 
-//EMAIL ____________________________________________
-// const nodemailer = require("nodemailer");
-// nodemailer.createTestAccount;
-// (error, account) => {
-//   const htmlEmail =
-//     "<h3>Email enviado desde react</h3> <ul> <li>Email: javipalacios@gmail.com</li><li>Asunto: test</li></ul> <h3>Mensaje</h3> <>cuerpo del mensaje</>";
-// };
-// let transporter = nodemailer.createTransport({
-//   host: "",
-//   port: "",
-//   auth: { user: "", pass: "" },
-// });
-// let mail = {
-//   from: "",
-//   to: "",
-//   replyTo: "",
-//   subject: "",
-//   text: "",
-//   html: htmlEmail,
-// };
-// transporter.sendMail(mail, (err, info) => {});
-// _____________________________________________
-// SERVE IMG____________________________________
-// app.get("/images", (req, res) => {
-//   // Setting the headers
-//   res.writeHead(200, {
-//     "Content-Type": "image/png",
-//   });
-
-//   // Reading the file
-//   fs.readFile(`static`, function (err, content) {
-//     // Serving the image
-//     res.end(content);
-//   });
-// });
-// ____________________________________________
-
 app.use("/", require("./routes"));
 
 app.listen(PORT, () => {
