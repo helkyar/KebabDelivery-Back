2b10caaae878ae36a4aba765b068cb4035b8a939
Borja Muñoz <90717945+borjamrd@users.noreply.github.com>
2022-05-21 09:46:30 +0200

    add basic  route to test back in aws

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.use(require("../middlewares/token"));
 // Other routes _____________________________________
 router.use("/users", require("./users"));
 router.use("/deliverer", require("./deliverer"));
+router.get('/hello', (req, res, next)=>{
+    res.render('Hola!',{name: 'Kometa App'})
+})
 
 module.exports = router;
