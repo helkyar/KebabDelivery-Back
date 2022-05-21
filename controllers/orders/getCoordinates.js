87d5ee5f03c5c6637f38a45d0af1c5a7f374a97a
Javier Palacios <javierpalaciosbotejara@gmail.com>
2022-05-21 02:38:36 +0200

    Fix maps and csv

--- controllers/orders/getCoordinates.js
+++ controllers/orders/getCoordinates.js
@@ -1,7 +1,7 @@
 const OrderManager = require(`../../${process.env.MANAGER}/OrderManager`);
 const DelivererManager = require(`../../${process.env.MANAGER}/DelivererManager`);
 
-async function getCoordinates(req, res) {  
+async function getCoordinates(req, res) {
   const data = req.params;
   //(!) Validation
   let latitude;
@@ -9,25 +9,25 @@ async function getCoordinates(req, res) {
 
   const order = await OrderManager.find(data);
 
-  if(!order || order[0]){
+  if (!order || !order[0]) {
     res.status(404).json({ error: "Doesn't exist" });
   } else if (order[0].state == 1) {
     latitude = order[0].from;
-    longitude= "";
+    longitude = "";
   } else if (order[0].state == 4) {
     latitude = order[0].to;
-    longitude= "";
+    longitude = "";
   }
 
-  const position = await DelivererManager.findByValue({pakage: data.id})
-  
-  if(position && position[0]){      
+  const position = await DelivererManager.findByValue({ pakage: data.id });
+
+  if (position && position[0]) {
     latitude = position[0].latitude;
-    longitude = position[0].longitude;    
+    longitude = position[0].longitude;
   }
 
   position
-    ? res.status(200).json({latitude, longitude})
+    ? res.status(200).json({ latitude, longitude, state: order[0].state })
     : res.status(404).json({ error: "Doesn't exist" });
 }
 
